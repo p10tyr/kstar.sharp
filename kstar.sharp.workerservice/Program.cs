f3d27fc076c32abc252c632db6a054626e214aa0
Piotr Kula <pkula@live.co.uk>
2022-02-01 21:20:06 +0000

    upgraded console to net6 LTS
    added workerservice so I can run this in linux 24/7 (or windows) as a background task.
    
    Mostly intetersted in mqtt working now

---
+++ kstar.sharp.workerservice/Program.cs
@@ -0,0 +1,10 @@
+using kstar.sharp.workerservice;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
